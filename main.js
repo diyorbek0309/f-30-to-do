872b4ea43145838e85d7e7612c969984911e15ea
Diyorbek Olimov <60889787+diyorbek0309@users.noreply.github.com>
2023-05-17 21:57:18 +0500

    finished complete and delete

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
 const addTitle = document.getElementById("addTitle");
 const titleInput = document.getElementById("titleInput");
 const todosWrap = document.getElementById("todosWrap");
-const todos = [];
+const editModal = document.getElementById("editModal");
+const editInput = document.getElementById("editInput");
+const editOK = document.getElementById("editOK");
+const editCancel = document.getElementById("editCancel");
+let todos = [];
+
+const showTodo = () => {
+  todosWrap.innerHTML = null;
+
+  todos.forEach((todo) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<strong>${
+      todo.isCompleted ? "<del>" + todo.title + "</del>" : todo.title
+    }</strong><span><button>E</button><button onclick='completeTodo(${
+      todo.id
+    })'>C</button><button onclick='deleteTodo(${todo.id})'>D</button></span>`;
+
+    todosWrap.appendChild(li);
+  });
+};
 
 const addTodo = () => {
-  const li = document.createElement("li");
   const todoItem = {
     id: todos.length,
     title: titleInput.value,
     isCompleted: false,
   };
   todos.push(todoItem);
+  showTodo();
 
-  li.innerHTML = `<strong>${titleInput.value}</strong><span><button>E</button><button onclick='completeTodo(${todoItem.id})'>C</button><button>D</button></span>`;
-
-  todosWrap.appendChild(li);
   titleInput.value = "";
 };
 
 const completeTodo = (id) => {
-  console.log(id);
+  todos[id].isCompleted = !todos[id].isCompleted;
+  showTodo();
 };
+
+const deleteTodo = (id) => {
+  todos = todos.filter((todo) => todo.id !== id);
+  showTodo();
+};
+
+const editTodo = (id) => {};
